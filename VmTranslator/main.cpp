0a770f1fb10f7dc6bee7a8482d667aebc9d898c4
Takuro Furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2021-12-17 00:27:05 +0900

    add abort when target dir is not found

--- VmTranslator/main.cpp
+++ VmTranslator/main.cpp
@@ -8,9 +8,11 @@ std::vector<std::string> find_vm_files(const std::string& target_name) {
 	//search direcotry
 	std::string dir_name = ".\\\\" + target_name;
 	std::vector<std::string> paths;
-	for (const auto& entry : std::filesystem::directory_iterator(dir_name)) {
-		if (entry.path().string().find_last_of(".vm") == entry.path().string().size() - 1) {
-			paths.push_back(entry.path().string());
+	if (std::filesystem::exists(dir_name)) {
+		for (const auto& entry : std::filesystem::directory_iterator(dir_name)) {
+			if (entry.path().string().find_last_of(".vm") == entry.path().string().size() - 1) {
+				paths.push_back(entry.path().string());
+			}
 		}
 	}
 	//search .vm file
@@ -25,17 +27,44 @@ std::vector<std::string> find_vm_files(const std::string& target_name) {
 
 int main(int argc, char** argv) {
 	std::string target_name;
-	std::cout << "Enter the name of a single .vm file or directory that contains .vm files.: ";
+	std::cout << "Enter the name of the target file/directory:";
 	std::cin >> target_name;
 	//find .vm file
 	auto paths = find_vm_files(target_name);
-	for (auto& path : paths) {
-		std::ifstream ifs(path);
-		std::string line;
-		std::getline(ifs, line);
-		std::cout << line << std::endl;
-		ifs.close();
-	}
 
+	if (paths.empty()) {
+		std::cerr << "No target file was detected." << std::endl;
+		return -1;
+	}
+	std::cout << "Target file(s) detected." << std::endl;
+	for (auto&& path : paths) {
+		std::cout << path << std::endl;
+	}
+	std::cout << std::endl;
+	CodeWriter writer;
+	writer.setFileName(target_name);
+	for (auto&& path : paths) {
+		Parser parser(path);
+		std::cout << "Parser opened." << std::endl;
+		while (parser.hasMoreCommands()) {
+			parser.advance();
+			std::cout << parser.currentCommand() << std::endl;
+			switch (parser.commandType()) {
+			case CmdType::C_ARITHMETIC:
+				writer.writeArithmetic(parser.arg1());
+				break;
+			case CmdType::C_POP:
+				writer.writePop(parser.arg1(), parser.arg2());
+				break;
+			case CmdType::C_PUSH:
+				writer.writePush(parser.arg1(), parser.arg2());
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	writer.close();
 	return 0;
 }
