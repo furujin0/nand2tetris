3938cec24b3e062af6b39786a77e2832419a91c8
Takuro Furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2021-12-23 18:52:20 +0900

    fix compiler.SymbolTable

--- Compiler/symbolTable.hpp
+++ Compiler/symbolTable.hpp
@@ -7,29 +7,29 @@
 struct IdInfo {
 	std::string name;
 	std::string type;
-	int index;
 	ID_KIND kind;
+	int index;
 };
 
 class SymbolTable {
 
+	int _next_idx = 0;
 	std::unordered_map<std::string, IdInfo> _table;
 public:
-	SymbolTable();
 
 	void startSubroutine();
 
-	int define(
+	void define(
 		const std::string& name,
 		const std::string& type,
 		ID_KIND kind
 	);
 
-	int varCount(ID_KIND kind);
+	int varCount(ID_KIND kind) const;
 
-	ID_KIND kindOf(const std::string& name);
+	ID_KIND kindOf(const std::string& name) const;
 
-	std::string typeOf(const std::string& name);
+	std::string typeOf(const std::string& name) const;
 
-	int indexOf(const std::string& name);
+	int indexOf(const std::string& name) const;
 };
